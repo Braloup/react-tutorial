529c4143820e5d71bc3cf8916219a00acba9b638
VieuxLoupGris <provocemois@gmail.com>
2019-03-21 11:51:10 +0100

    Updates

---
+++ precache-manifest.e9207944f8eca20b7beea4eacfa23170.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0b8e0c3ca94a24ec31fd",
+    "url": "/static/js/main.bd01aec5.chunk.js"
+  },
+  {
+    "revision": "37633691b9724b784dbc",
+    "url": "/static/js/2.ff439d2d.chunk.js"
+  },
+  {
+    "revision": "0b8e0c3ca94a24ec31fd",
+    "url": "/static/css/main.d1ffa482.chunk.css"
+  },
+  {
+    "revision": "44be0f59deeff3b3a2885f756ec3180f",
+    "url": "/index.html"
+  }
+];
